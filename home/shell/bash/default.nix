34db5d44f5bdc914580e88bc819b839acc79e157
lukasfriedhoff <155996615+lukasfriedhoff@users.noreply.github.com>
2025-10-06 16:18:19 +0200

    fix: docker compose alias

--- home/shell/bash/default.nix
+++ home/shell/bash/default.nix
@@ -16,7 +16,7 @@
     shellAliases = {
       k = "kubectl";
       tf = "terraform";
-      dc = "docker-compose";
+      dc = "docker compose";
       urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
       urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
     };
