24a30e803e20a5b6fd8eeb2d3cfbeec7633cfc23
lukasfriedhoff <155996615+lukasfriedhoff@users.noreply.github.com>
2025-11-07 19:33:28 +0100

    fix: checksum

--- home/programs/stylix/default.nix
+++ home/programs/stylix/default.nix
@@ -12,7 +12,7 @@ in {
     # Prefetch wallpaper via stylix to keep switches pure. Swap to a local file if you prefer.
     image = pkgs.fetchurl {
       url = "https://wallpapercave.com/wp/wp9218666.jpg";
-      sha256 = "2467657648a269a1bf3aa7be4308642d371bf8cc063a7a1b86731c3d81c9508e";
+      sha256 = "sha256-rB3wJm+aF7zWUYQAYouWwikWYHPFZZHeKQXK+wlZqRE=";
       curlOptsList = [ "-HUser-Agent: Wget/1.21.4" ];
     };
     base16Scheme = theme;
