9df9686a15b3dfffb2541b4d7d22fda4f30d9703
lukasfriedhoff <155996615+lukasfriedhoff@users.noreply.github.com>
2025-11-12 13:51:35 +0100

    fix: vscode on intel gpu

--- home/platforms/linux/default.nix
+++ home/platforms/linux/default.nix
@@ -8,4 +8,23 @@
       element-desktop
     ])
   );
+
+  xdg.desktopEntries."code" = lib.mkIf (!pkgs.stdenv.isDarwin) {
+    name = "Visual Studio Code (Intel)";
+    exec = "env DRI_PRIME=0 __NV_PRIME_RENDER_OFFLOAD=0 __GLX_VENDOR_LIBRARY_NAME=modesetting code %F";
+    icon = "code";
+    genericName = "Text Editor";
+    categories = [ "Utility" "TextEditor" "Development" "IDE" ];
+    terminal = false;
+    type = "Application";
+    mimeType = [
+      "text/plain"
+      "inode/directory"
+      "application/x-code-workspace"
+      "text/x-python"
+      "text/x-c++src"
+    ];
+    startupNotify = true;
+    comment = "Code editing. Redefined.";
+  };
 }
