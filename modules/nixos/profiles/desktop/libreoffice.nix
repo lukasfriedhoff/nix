d16b3d091b206a44f266b224ecb769ce5d9da44f
lukasfriedhoff <155996615+lukasfriedhoff@users.noreply.github.com>
2025-11-15 10:26:10 +0100

    feat: libreoffice light theme

--- modules/nixos/profiles/desktop/libreoffice.nix
+++ modules/nixos/profiles/desktop/libreoffice.nix
@@ -20,14 +20,34 @@ in
 {
   options.desktop.libreoffice.enable = lib.mkEnableOption "LibreOffice office suite";
 
-  config = lib.mkMerge [
-    (lib.mkIf autoEnable {
-      desktop.libreoffice.enable = lib.mkDefault true;
-    })
-    (lib.mkIf cfg.enable {
-      environment.systemPackages = [
-        pkgs.libreoffice
-      ];
-    })
-  ];
+  config =
+    let
+      # LibreOffice renders spreadsheets with dark backgrounds when the global
+      # theme is dark, which makes text illegible. Force a light GTK theme for
+      # every entry point by wrapping the shipped binaries.
+      libreOfficeLight =
+        pkgs.symlinkJoin {
+          name = "libreoffice-light";
+          paths = [ pkgs.libreoffice ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            for exe in "$out"/bin/*; do
+              if [ -x "$exe" ] && [ ! -d "$exe" ]; then
+                wrapProgram "$exe" \
+                  --set GTK_THEME Adwaita:light \
+                  --set SAL_FORCE_HC 0 \
+                  --set SAL_USE_VCLPLUGIN gtk3
+              fi
+            done
+          '';
+        };
+    in
+    lib.mkMerge [
+      (lib.mkIf autoEnable {
+        desktop.libreoffice.enable = lib.mkDefault true;
+      })
+      (lib.mkIf cfg.enable {
+        environment.systemPackages = [ libreOfficeLight ];
+      })
+    ];
 }
