5eb3efa37562716af3bc19d38bc64f59d9db32f0
lukasfriedhoff <155996615+lukasfriedhoff@users.noreply.github.com>
2025-11-21 20:32:11 +0100

    asd

--- resources/ssh/keys.nix
+++ resources/ssh/keys.nix
@@ -1,18 +1,62 @@
 [
-  { secret = "ssh/chaospott.priv"; path = ".ssh/personal/chaospott/noc"; }
-  { secret = "ssh/chaospott_door.priv"; path = ".ssh/personal/chaospott/door"; }
-  { secret = "ssh/ci.priv"; path = ".ssh/personal/ci"; }
-  { secret = "ssh/cloud-init.priv"; path = ".ssh/personal/cloud-init"; }
-  { secret = "ssh/docker-host.priv"; path = ".ssh/personal/docker-host"; }
-  { secret = "ssh/h4xxsrv1.priv"; path = ".ssh/personal/h4xxsrv1"; }
-  { secret = "git-personal-ed25519.priv"; path = ".ssh/personal/id_ed25519"; }
-  { secret = "ssh/ionos1.priv"; path = ".ssh/personal/ionos1"; }
-  { secret = "ssh/ionos2.priv"; path = ".ssh/personal/ionos2"; }
-  { secret = "ssh/labrouter.priv"; path = ".ssh/personal/labrouter"; }
-  { secret = "ssh/nas01.priv"; path = ".ssh/personal/nas01"; }
-  { secret = "ssh/nas01-unlock.priv"; path = ".ssh/personal/nas01-unlock"; }
-  { secret = "ssh/router01.h4xx.local.priv"; path = ".ssh/personal/router01.h4xx.local"; }
-  { secret = "ssh/storage01.priv"; path = ".ssh/personal/storage01"; }
-  { secret = "ssh/srv1.priv"; path = ".ssh/personal/srv1"; }
-  { secret = "ssh/srv4.priv"; path = ".ssh/personal/srv4"; }
+  {
+    secret = "ssh/chaospott/noc.priv";
+    path = ".ssh/personal/chaospott/noc";
+  }
+  {
+    secret = "ssh/chaospott_door.priv";
+    path = ".ssh/personal/chaospott/door";
+  }
+  {
+    secret = "ssh/ci.priv";
+    path = ".ssh/personal/ci";
+  }
+  {
+    secret = "ssh/cloud-init.priv";
+    path = ".ssh/personal/cloud-init";
+  }
+  {
+    secret = "ssh/docker-host.priv";
+    path = ".ssh/personal/docker-host";
+  }
+  {
+    secret = "ssh/h4xxsrv1.priv";
+    path = ".ssh/personal/h4xxsrv1";
+  }
+  {
+    secret = "git-personal-ed25519.priv";
+    path = ".ssh/personal/id_ed25519";
+  }
+  {
+    secret = "ssh/ionos1.priv";
+    path = ".ssh/personal/ionos1";
+  }
+  {
+    secret = "ssh/ionos2.priv";
+    path = ".ssh/personal/ionos2";
+  }
+  {
+    secret = "ssh/labrouter.priv";
+    path = ".ssh/personal/labrouter";
+  }
+  {
+    secret = "ssh/nas01.priv";
+    path = ".ssh/personal/nas01";
+  }
+  {
+    secret = "ssh/nas01-unlock.priv";
+    path = ".ssh/personal/nas01-unlock";
+  }
+  {
+    secret = "ssh/storage01.priv";
+    path = ".ssh/personal/storage01";
+  }
+  {
+    secret = "ssh/srv1.priv";
+    path = ".ssh/personal/srv1";
+  }
+  {
+    secret = "ssh/srv4.priv";
+    path = ".ssh/personal/srv4";
+  }
 ]
