9dfe74bbf03b0ddce053ca9c976427c9f84b8765
lukasfriedhoff <155996615+lukasfriedhoff@users.noreply.github.com>
2026-01-10 20:51:19 +0100

    fix(ceph): remove duplicate ceph/ prefix from lockbox key paths
    The resolveSecret function already prepends secrets.ceph directory,
    so paths should be relative to that, not include redundant ceph/ prefix.

--- resources/homelab/disks.nix
+++ resources/homelab/disks.nix
@@ -2,31 +2,31 @@
   "ata-T-FORCE_1TB_TPBF2208240050700434" = {
     host = "srv1";
     purpose = "ceph";
-    lockboxKeyFile = "ceph/83897024-e964-11f0-9d5c-0cc47a6c3802/osd-lockbox/ata-T-FORCE_1TB_TPBF2208240050700434.key";
+    lockboxKeyFile = "83897024-e964-11f0-9d5c-0cc47a6c3802/osd-lockbox/ata-T-FORCE_1TB_TPBF2208240050700434.key";
     type = "ssd";
   };
   "ata-T-FORCE_1TB_TPBF2208240050700816" = {
     host = "srv1";
     purpose = "ceph";
-    lockboxKeyFile = "ceph/83897024-e964-11f0-9d5c-0cc47a6c3802/osd-lockbox/ata-T-FORCE_1TB_TPBF2208240050700816.key";
+    lockboxKeyFile = "83897024-e964-11f0-9d5c-0cc47a6c3802/osd-lockbox/ata-T-FORCE_1TB_TPBF2208240050700816.key";
     type = "ssd";
   };
   "ata-T-FORCE_1TB_TPBF2209020040601421" = {
     host = "srv1";
     purpose = "ceph";
-    lockboxKeyFile = "ceph/83897024-e964-11f0-9d5c-0cc47a6c3802/osd-lockbox/ata-T-FORCE_1TB_TPBF2209020040601421.key";
+    lockboxKeyFile = "83897024-e964-11f0-9d5c-0cc47a6c3802/osd-lockbox/ata-T-FORCE_1TB_TPBF2209020040601421.key";
     type = "ssd";
   };
   "ata-T-FORCE_1TB_TPBF2209020040602776" = {
     host = "srv1";
     purpose = "ceph";
-    lockboxKeyFile = "ceph/83897024-e964-11f0-9d5c-0cc47a6c3802/osd-lockbox/ata-T-FORCE_1TB_TPBF2209020040602776.key";
+    lockboxKeyFile = "83897024-e964-11f0-9d5c-0cc47a6c3802/osd-lockbox/ata-T-FORCE_1TB_TPBF2209020040602776.key";
     type = "ssd";
   };
   "ata-T-FORCE_1TB_TPBF2209130010100025" = {
     host = "srv1";
     purpose = "ceph";
-    lockboxKeyFile = "ceph/83897024-e964-11f0-9d5c-0cc47a6c3802/osd-lockbox/ata-T-FORCE_1TB_TPBF2209130010100025.key";
+    lockboxKeyFile = "83897024-e964-11f0-9d5c-0cc47a6c3802/osd-lockbox/ata-T-FORCE_1TB_TPBF2209130010100025.key";
     type = "ssd";
   };
 }
