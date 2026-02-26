22d9ee93cd5f51fd7589e6c35772254fe8717651
lukasfriedhoff <155996615+lukasfriedhoff@users.noreply.github.com>
2026-02-26 18:35:08 +0100

    feat(srv2): align vlan bridges and initrd dhcp client-id

--- hosts/homelab/srv2/configuration.nix
+++ hosts/homelab/srv2/configuration.nix
@@ -1,7 +1,12 @@
 {
   inputs,
+  lib,
   ...
 }:
+
+let
+  nets = import ../../../resources/homelab/networks.nix;
+in
 
 {
   imports = [
@@ -13,6 +18,72 @@
   ];
 
   networking.hostName = "srv2";
+  networking.useNetworkd = true;
+  networking.networkmanager.enable = false;
+  networking.defaultGateway = lib.mkForce null;
+  networking.nameservers = [ "10.1.30.1" ];
+  networking.interfaces.enp1s0.useDHCP = lib.mkForce false;
+  networking.vlans = {
+    "enp1s0.20" = {
+      inherit (nets.vlans.server) id;
+      interface = "enp1s0";
+    };
+    "enp1s0.40" = {
+      inherit (nets.vlans.storage) id;
+      interface = "enp1s0";
+    };
+    "enp1s0.10" = {
+      inherit (nets.vlans.lan) id;
+      interface = "enp1s0";
+    };
+    "enp1s0.12" = {
+      inherit (nets.vlans.iot) id;
+      interface = "enp1s0";
+    };
+    "enp1s0.13" = {
+      inherit (nets.vlans.windows) id;
+      interface = "enp1s0";
+    };
+    "enp1s0.50" = {
+      inherit (nets.vlans.lab) id;
+      interface = "enp1s0";
+    };
+  };
+
+  # Bring VLAN subinterfaces up even without an IP so bridges attach cleanly.
+  networking.interfaces."enp1s0.20".useDHCP = false;
+  networking.interfaces."enp1s0.40".useDHCP = false;
+  networking.interfaces."enp1s0.10".useDHCP = false;
+  networking.interfaces."enp1s0.12".useDHCP = false;
+  networking.interfaces."enp1s0.13".useDHCP = false;
+  networking.interfaces."enp1s0.50".useDHCP = false;
+
+  # Libvirt-friendly bridges for each VLAN (mgmt on brvlan30).
+  networking.bridges = {
+    brvlan10.interfaces = [ "enp1s0.10" ];
+    brvlan12.interfaces = [ "enp1s0.12" ];
+    brvlan13.interfaces = [ "enp1s0.13" ];
+    brvlan20.interfaces = [ "enp1s0.20" ];
+    brvlan30.interfaces = [ "enp1s0" ]; # untagged mgmt
+    brvlan40.interfaces = [ "enp1s0.40" ];
+    brvlan50.interfaces = [ "enp1s0.50" ];
+  };
+  networking.interfaces.brvlan30 = {
+    useDHCP = true;
+    macAddress = "68:1d:ef:39:95:b2";
+  };
+  networking.interfaces.brvlan20.useDHCP = true;
+  networking.interfaces.brvlan40.useDHCP = true;
+  networking.hosts = {
+    "127.0.0.2" = lib.mkForce [ ];
+  };
+
+  # Use MAC-based DHCP client ID on the management bridge.
+  systemd.network.networks."30-brvlan30" = {
+    matchConfig.Name = "brvlan30";
+    networkConfig.DHCP = "yes";
+    dhcpV4Config.ClientIdentifier = "mac";
+  };
 
   homelab.personalServer = {
     enable = true;
@@ -28,7 +99,30 @@
     authorizedKeyFile = ./initrd-authorized.pub;
   };
 
+  boot.initrd.network.udhcpc.enable = true;
+  boot.initrd.network.udhcpc.extraArgs = [
+    "-x"
+    "clientid:01:68:1d:ef:39:95:b2"
+    "-x"
+    "hostname:srv2"
+  ];
+
+  boot.initrd.availableKernelModules = lib.mkAfter [
+    "e1000e"
+    "igb"
+    "igc"
+    "ixgbe"
+    "r8169"
+    "r8152"
+  ];
+
   networking.extraHosts = ''
     # srv2 srv2.lab.h4xx.io 10.42.1.91
   '';
+
+  users.users.lukasf = {
+    isNormalUser = true;
+    group = "users";
+    hashedPassword = "$6$yzoypuzQDaJPoH3Q$jMjF9ciENiSRMMDfkeJJdGb9jMK1W35kNLvO3gH4B58rhWj285gYBI6n8.i8ry8jG5f7Ll3VxNbdvX5Sp2aGs0";
+  };
 }
