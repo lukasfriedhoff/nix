05120739f237072c146336dc63557fe909e82da0
lukasfriedhoff <155996615+lukasfriedhoff@users.noreply.github.com>
2026-03-01 12:42:20 +0100

    fix(hosts): use server sops age key path for homelab

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -8,15 +8,17 @@
 let
   desktopDefaults = config.networking.networkmanager.enable;
   homelabDefaults = config.homelab.personalServer.enable;
-  needsSopsKey = desktopDefaults || homelabDefaults;
 in
 {
   config = lib.mkMerge [
     {
       lukasf.remoteBuilds.publicHostKey = lib.mkDefault "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSURZeXEvNm9XNS9vTkhMazZOM1FLaWFjSVBnaEkrdW9VTlY1T0MyRXI0YUEgcm9vdEBuaXhvcwo=";
     }
-    (lib.mkIf needsSopsKey {
+    (lib.mkIf desktopDefaults {
       sops.age.keyFile = lib.mkDefault "/home/lukasf/.config/sops/age/keys.txt";
+    })
+    (lib.mkIf homelabDefaults {
+      sops.age.keyFile = lib.mkDefault "/var/lib/sops-nix/age/keys.txt";
     })
     (lib.mkIf desktopDefaults {
       shared.network.resolved = {
