7d9731ad60f94ae539cb38d3d290c729910bb55d
lukasfriedhoff <155996615+lukasfriedhoff@users.noreply.github.com>
2026-06-05 12:20:55 +0200

    fix(desktop): pin master pdf editor to compatible version

--- modules/features/profile/desktop/home.nix
+++ modules/features/profile/desktop/home.nix
@@ -65,6 +65,33 @@ let
   isLinuxDesktop = desktopEnabled && (!pkgs.stdenv.isDarwin);
   isPersonalWorkstation = profile != null && lib.elem profile personalWorkstationProfiles;
   isLinuxWorkstation = isPersonalWorkstation && (!pkgs.stdenv.isDarwin);
+
+  masterpdfeditorLicenseCompatible = pkgs.masterpdfeditor.overrideAttrs (_old: {
+    version = "5.9.60";
+    src = pkgs.fetchurl {
+      url = "https://code-industry.net/public/master-pdf-editor-5.9.60-qt5.x86_64.tar.gz";
+      hash = "sha256-KnqqoPhpcQA3mFuuGlZO6RyONgbKFDojDFz+hYFfq9c=";
+    };
+
+    installPhase = ''
+      runHook preInstall
+
+      substituteInPlace masterpdfeditor5.desktop \
+        --replace-fail "Exec=/opt/master-pdf-editor-5/masterpdfeditor5" "Exec=masterpdfeditor5" \
+        --replace-fail "Path=/opt/master-pdf-editor-5" "Path=$out/share/masterpdfeditor" \
+        --replace-fail "/opt/master-pdf-editor-5/masterpdfeditor5.png" "masterpdfeditor5"
+
+      install -Dm644 masterpdfeditor5.desktop -t $out/share/applications
+      install -Dm644 masterpdfeditor5.png -t $out/share/icons/hicolor/128x128/apps
+      install -Dm755 masterpdfeditor5 -t $out/share/masterpdfeditor
+      cp -r stamps templates lang fonts $out/share/masterpdfeditor
+
+      mkdir -p $out/bin
+      ln -s $out/share/masterpdfeditor/masterpdfeditor5 $out/bin/masterpdfeditor5
+
+      runHook postInstall
+    '';
+  });
 in
 {
   config = lib.mkMerge [
@@ -125,7 +152,7 @@ in
         pkgs.go
         pkgs.gopls
         pkgs.gcc
-        pkgs.masterpdfeditor
+        masterpdfeditorLicenseCompatible
         pkgs.nodejs_22
         pkgs.noto-fonts-color-emoji
         pkgs.font-awesome
