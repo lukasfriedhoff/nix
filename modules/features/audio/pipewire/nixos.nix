20c6b28d76a42555a1828470c84ca5235c70486a
lukasfriedhoff <155996615+lukasfriedhoff@users.noreply.github.com>
2026-07-02 20:15:06 +0200

    fix(audio): stabilize pipewire bluetooth profiles

--- modules/features/audio/pipewire/nixos.nix
+++ modules/features/audio/pipewire/nixos.nix
@@ -26,6 +26,43 @@ in
       alsa.enable = true;
       alsa.support32Bit = cfg.support32Bit;
       pulse.enable = true;
+      wireplumber.extraConfig."10-bluetooth-audio" = {
+        "monitor.bluez.properties" = {
+          "bluez5.roles" = [
+            "a2dp_sink"
+            "a2dp_source"
+            "bap_sink"
+            "bap_source"
+            "hsp_hs"
+            "hsp_ag"
+            "hfp_hf"
+            "hfp_ag"
+          ];
+          "bluez5.codecs" = [
+            "sbc"
+            "sbc_xq"
+            "aac"
+          ];
+          "bluez5.enable-sbc-xq" = true;
+          "bluez5.hfphsp-backend" = "native";
+        };
+        "device.profile.priority.rules" = [
+          {
+            matches = [
+              {
+                "device.name" = "~bluez_card.*";
+              }
+            ];
+            actions.update-props.priorities = [
+              "a2dp-sink-sbc_xq"
+              "a2dp-sink-aac"
+              "a2dp-sink-sbc"
+              "headset-head-unit"
+              "headset-head-unit-cvsd"
+            ];
+          }
+        ];
+      };
       extraConfig."pipewire-pulse"."10-disable-stream-restore" = {
         "pulse.properties" = {
           "pulse.cmd.stream-restore" = false;
